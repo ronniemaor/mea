ca72b1138423b8b0ceca1b0899e63767425f518c
Ronnie Maor <ronnie.maor@gmail.com>
2013-05-28 22:00:49 +0300

    changed how data is loaded

--- loadData.m
+++ loadData.m
@@ -6,6 +6,25 @@ function data = loadData(sessionKey, bSilent)
     if ~bSilent
         fprintf('Loading %s (from %s)\n', sessionKey, dataFile)
     end
-    data = load(dataFile);
+    data = struct;
     data.sessionKey = sessionKey;
+    fileData = load(dataFile);
+    data = addFromFile(data,fileData);
+end
+
+function data = addFromFile(data,fileData)
+    data.channelNumbers = [];
+    data.channels = cell(1,0);
+    nCells = 0;
+    allFields = fieldnames(fileData);
+    for i = 1:length(allFields)
+        f = allFields{i};
+        ch = fileData.(f);
+        chNum = str2double(f(4:end));
+        if ~isempty(ch)
+            nCells = nCells + 1;
+            data.channelNumbers(nCells) = chNum;
+            data.channels{nCells} = ch;
+        end
+    end
 end
