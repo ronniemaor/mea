c5136c4c42c04e9767f9ab5ffc724bed3b740ab5
Ronnie Maor <ronnie.maor@gmail.com>
2013-05-31 15:27:50 +0300

    flat unit spike times

--- loadData.m
+++ loadData.m
@@ -15,16 +15,28 @@ end
 function data = addFromFile(data,fileData)
     data.channelNumbers = [];
     data.channels = cell(1,0);
-    nCells = 0;
+    data.unitIds = [];
+    data.unitSpikeTimes = cell(1,0);
+    nChannels = 0;
+    nUnits = 0;
     allFields = fieldnames(fileData);
-    for i = 1:length(allFields)
-        f = allFields{i};
+    for iField = 1:length(allFields)
+        f = allFields{iField};
         ch = fileData.(f);
         chNum = str2double(f(4:end));
-        if ~isempty(ch)
-            nCells = nCells + 1;
-            data.channelNumbers(nCells) = chNum;
-            data.channels{nCells} = ch;
+        if isempty(ch)
+            continue
+        end
+        nChannels = nChannels + 1;
+        data.channelNumbers(nChannels) = chNum;
+        data.channels{nChannels} = ch;
+        units = unique(ch(:,2));
+        for iUnit = 1:length(units)
+            unitNum = units(iUnit);
+            nUnits = nUnits + 1;
+            data.unitIds(nUnits,1:2) = [chNum unitNum];
+            unitRows = ch(:,2) == unitNum;
+            data.unitSpikeTimes{nUnits} = ch(unitRows,3);
         end
     end
 end
