1592058eb7fe34e080249c35e817725f079a5037
Ronnie Maor <ronnie.maor@gmail.com>
2013-06-01 11:49:27 +0300

    mUnitPlot

---
+++ mUnitPlot.m
@@ -0,0 +1,15 @@
+function mUnitPlot(data, f, ttl)
+    nPlots = data.nUnits;
+    nRows = ceil(sqrt(nPlots));
+    nCols = ceil(nPlots/nRows);
+    figure;
+    for iUnit=1:data.nUnits
+        ISIs = calcISIs(data.unitSpikeTimes{iUnit});
+        stats = cellfun(f, ISIs);
+        subplot(nRows,nCols,iUnit)
+        plot(stats);
+    end
+    if exist('ttl','var')
+        topLevelTitle(ttl)
+    end
+end
--- snippets.m
+++ snippets.m
@@ -6,11 +6,17 @@ figure; plot(firingRates(data.unitSpikeTimes{1}));
 
 %%
 ISIs = calcISIs(data.unitSpikeTimes{1});
+
+%%
 figure; plot(cellfun(@(x) 1/mean(x), ISIs)); % same as firingRates
 
 figure; plot(cellfun(@(x) 1/median(x), ISIs));
 figure; plot(cellfun(@(x) median(x), ISIs));
 figure; plot(cellfun(@(x) log(std(x)), ISIs));
+figure; plot(cellfun(@(x) std(x)/mean(x), ISIs)); % CV
+
+%%
+mUnitPlot(data, @(x) std(x)/mean(x), 'CV(ISI)')
 
 %%
 figNetworkRates()
