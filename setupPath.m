c0a60e05f8afcf17aa940863ae8bf3c32ac824ee
Ronnie Maor <ronnie.maor@gmail.com>
2013-06-16 23:11:34 +0300

    added copy of lab code (take_from_struct etc)

--- setupPath.m
+++ setupPath.m
@@ -1,6 +1,7 @@
 function setupPath()
     codeDir = fileparts(mfilename('fullpath'));
     path(path,codeDir);
+    path(path,[codeDir '/lab']);
     path(path,[codeDir '/3rd party']);
     path(path,[codeDir '/util']);
     path(path,[codeDir '/figures']);
