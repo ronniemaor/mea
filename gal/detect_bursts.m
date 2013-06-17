501c1d45f6f7388f733a29c0f3790907c9d658db
Ronnie Maor <ronnie.maor@gmail.com>
2013-06-17 08:06:15 +0300

    minor - removed warnings

--- gal/detect_bursts.m
+++ gal/detect_bursts.m
@@ -27,8 +27,8 @@ function [burst_times, pvalues] = detect_bursts_gamma(burst_mode, trains, parms)
   times = estimate_bin_sec*ceil(times/estimate_bin_sec);
   rate = sparse(times, ones(size(times)), ones(size(times)));
   
-  num_bins_in_epoch = collect_bin_sec / estimate_bin_sec;
-  num_epochs = length(rate) / num_bins_in_epoch;
+  num_bins_in_epoch = floor(collect_bin_sec / estimate_bin_sec);
+  num_epochs = floor(length(rate) / num_bins_in_epoch);
 
   bins = [0:1:500];
   pvalues = cell(num_epochs, 1);
