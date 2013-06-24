86e016d465c7785fb4ac123cae179fb32f9fce64
Ronnie Maor <ronnie.maor@gmail.com>
2013-06-24 12:14:52 +0300

    numActiveDist - added option of not using subplots

--- numActiveDist.m
+++ numActiveDist.m
@@ -1,5 +1,6 @@
 function numActiveDist(data,parms)
     if ~exist('parms','var'); parms = make_parms(); end
+    bSubplots = take_from_struct(parms,'subplots',0);
 
     nHours = data.maxUnitFullHours;    
     
@@ -18,17 +19,33 @@ function numActiveDist(data,parms)
     pdfs = cellfun(f,pActive,'UniformOutput',0);
     maxP = max(cell2mat(pdfs));
     
-    figure;
-    nPlots = length(frames);
-    [nRows, nCols] = rectSubplot(nPlots);
-    for iPlot=1:nPlots
-        subplot(nRows,nCols,iPlot)
-        iHour = frames(iPlot);
-        bar(edges,pdfs{iHour});
-        xlim([0 1])
-        ylim([0 maxP])
-        set(gca,'XTick',[])
-        set(gca,'YTick',[])
-        title(sprintf('hour %d (%.0fHz)', iHour, rates(iHour)))
+    figure;    
+
+    if bSubplots
+        nPlots = length(frames);
+        [nRows, nCols] = rectSubplot(nPlots);
+        for iPlot=1:nPlots
+            subplot(nRows,nCols,iPlot); set(gca, 'FontSize', 16)
+            iHour = frames(iPlot);
+            bar(edges,pdfs{iHour});
+            xlim([0 1])
+            ylim([0 maxP])
+            set(gca,'XTick',[])
+            set(gca,'YTick',[])
+            title(sprintf('hour %d (%.0fHz)', iHour, rates(iHour)))
+        end
+        topLevelTitle(sprintf('Distribution of active-units-fraction - %s (bin=%.1f sec)', data.sessionKey, tBaseBin))
+    else
+        set(gca, 'FontSize', 16);
+        nPlots = length(frames);
+        for iPlot=1:nPlots
+            iHour = frames(iPlot);
+            plot(edges,pdfs{iHour}, 'LineWidth', 2); hold all;
+            xlim([0 1])
+            ylim([0 maxP])
+            title(sprintf('Distributions of active units at different times - %s', data.sessionKey))
+        end
+        formatLegend = @(iHour) sprintf('Hour %d',iHour);
+        legend(arrayfun(formatLegend, frames, 'UniformOutput', 0));
     end
-    topLevelTitle(sprintf('Distribution of active-units-fraction - %s (bin=%.1f sec)', data.sessionKey, tBaseBin))
+end
